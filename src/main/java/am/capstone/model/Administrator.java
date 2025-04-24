b82d94dcdc267ccc6976bad64783347114d4d383
kzakaryan <kzakaryan@griddynamics.com>
2025-04-24 22:15:20 +0400

    Adding model

---
+++ src/main/java/am/capstone/model/Administrator.java
@@ -0,0 +1,26 @@
+package am.capstone.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Administrator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long adminId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+
+}
---
+++ src/main/java/am/capstone/model/Instructor.java
@@ -0,0 +1,29 @@
+package am.capstone.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long instructorId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+
+    @OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Course> coursesAssigned;
+}
