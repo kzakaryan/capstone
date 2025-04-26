dfe38ec39740859fbc223e19f48ab6f6a989cd15
kzakaryan <kzakaryan@griddynamics.com>
2025-04-26 14:37:36 +0400

    Adding Controller Classes

---
+++ src/main/java/am/capstone/controller/StudentController.java
@@ -0,0 +1,9 @@
+package am.capstone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/students")
+@Controller
+public class StudentController {
+}
