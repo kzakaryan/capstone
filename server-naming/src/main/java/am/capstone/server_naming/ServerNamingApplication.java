e4d657d386b685b8f96b70fe8ffb1a9aaeb1945a
kzakaryan <kzakaryan@griddynamics.com>
2025-05-16 17:27:24 +0400

    Completing Service Discovery Task

---
+++ server-naming/src/main/java/am/capstone/server_naming/ServerNamingApplication.java
@@ -0,0 +1,15 @@
+package am.capstone.server_naming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerNamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerNamingApplication.class, args);
+	}
+
+}
---
+++ server-naming/src/test/java/am/capstone/server_naming/ServerNamingApplicationTests.java
@@ -0,0 +1,13 @@
+package am.capstone.server_naming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerNamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
