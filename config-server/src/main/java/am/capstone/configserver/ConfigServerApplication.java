afa33f8bdb589a01dc3eb622a7a2b4bf39367aab
kzakaryan <kzakaryan@griddynamics.com>
2025-05-29 16:32:47 +0400

    Adding config server

--- config-server/src/main/java/am/capstone/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/am/capstone/configserver/ConfigServerApplication.java
@@ -1,14 +1,15 @@
-package am.capstone;
+package am.capstone.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CapstoneApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CapstoneApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
+
 }
