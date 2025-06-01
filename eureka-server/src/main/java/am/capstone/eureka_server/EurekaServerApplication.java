50cf6d9e2c8b7ee5ec5ef86dcbfe203a3f6caac1
kzakaryan <kzakaryan@griddynamics.com>
2025-06-01 13:17:33 +0400

    Upds

--- eureka-server/src/main/java/am/capstone/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/am/capstone/eureka_server/EurekaServerApplication.java
@@ -1,16 +1,15 @@
-package am.capstone.server_naming;
+package am.capstone.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServerNamingApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerNamingApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
