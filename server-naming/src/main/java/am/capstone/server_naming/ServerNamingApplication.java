bee2a2148d24ce3f404726663216acb4f3949419
kzakaryan <kzakaryan@griddynamics.com>
2025-06-01 12:26:54 +0400

    Issues Resolved

--- server-naming/src/main/java/am/capstone/server_naming/ServerNamingApplication.java
+++ server-naming/src/main/java/am/capstone/server_naming/ServerNamingApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class ServerNamingApplication {
 
 	public static void main(String[] args) {
